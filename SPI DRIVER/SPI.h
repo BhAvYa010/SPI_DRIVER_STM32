d58828fc951495a68db5e28b9f0808e06472963e
Bhavya Ashwinbhai Patel <96838352+BhAvYa010@users.noreply.github.com>
2025-03-10 10:09:08 +0530

    Update SPI.h

--- SPI DRIVER/SPI.h
+++ SPI DRIVER/SPI.h
@@ -1,21 +1,17 @@
-#ifndef STM32L432KC
-#define STM32L432KC
-#include "stm32l432xx.h"
-#endif
+#ifndef SPI_H
+#define SPI_H
 
+#include "stm32l432xx.h"
+
+// Clock Initialization
 void initClocks(void);
 
+// Hardware Slave Management (HSM)
+void initSPI_HSM(void);
+uint8_t transferSPI_HSM(uint8_t tx_data);
+
+// Software Slave Management (SSM)
 void initSPI_SSM(void);
-void configSpi1Pins_SSM(void);
-void setPinMode_SSM(void);
-void setAF_SSM(void);
-void configSpi_SSM(void);
 uint8_t transferSPI_SSM(uint8_t tx_data);
 
-
-void initSPI_HSM(void);
-void configSpi1Pins_HSM(void);
-void setPinMode_HSM(void);
-void setAF_HSM(void);
-void configSpi_HSM(void);
-uint8_t transferSPI_HSM(uint8_t tx_data);
+#endif
